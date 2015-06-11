a07964affa6535a19b81e86c95b4bfaca6a234a8
Łukasz Sowa <contact@lukaszsowa.pl>
2015-06-11 19:40:52 +0200

    Implementing ProductRepository.

--- src/main/scala/checkout/product/ProductRepository.scala
+++ src/main/scala/checkout/product/ProductRepository.scala
@@ -1,5 +1,5 @@
 package checkout.product
 
 class ProductRepository(private val products: Seq[Product]) {
-  def findById(id: ProductId): Option[Product] = ???
+  def findById(id: ProductId): Option[Product] = products.find(_.id == id)
 }
