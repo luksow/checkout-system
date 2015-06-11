a6798fa993b517a9b07cce14d2142197f34149ad
Łukasz Sowa <contact@lukaszsowa.pl>
2015-06-11 20:14:42 +0200

    Adding CartServiceSpec.

---
+++ src/test/scala/checkout/cart/CartServiceSpec.scala
@@ -0,0 +1,25 @@
+package checkout.cart
+
+import checkout.product.{Price, Product, ProductId}
+import org.scalatest._
+
+class CartServiceSpec extends FlatSpec with Matchers {
+  val appleProduct = Product(ProductId("apple"), Price(BigDecimal("0.60")))
+  val orangeProduct = Product(ProductId("orange"), Price(BigDecimal("0.25")))
+  val cartService = new CartService
+
+  "Empty cart" should "be worth 0" in {
+    cartService.checkout(Cart()).total should equal(BigDecimal(0))
+  }
+
+  "Cart with products" should "calculate price correctly" in {
+    val cartWithApple = Cart().withProduct(appleProduct)
+    cartService.checkout(cartWithApple).total should equal(BigDecimal("0.60"))
+
+    val cartWithAppleAndOrange = Cart(Seq(appleProduct, orangeProduct))
+    cartService.checkout(cartWithAppleAndOrange).total should equal(BigDecimal("0.85"))
+
+    val cartWithManyProducts = Cart(Seq(appleProduct, appleProduct, orangeProduct, appleProduct))
+    cartService.checkout(cartWithManyProducts).total should equal(BigDecimal("2.05"))
+  }
+ }
