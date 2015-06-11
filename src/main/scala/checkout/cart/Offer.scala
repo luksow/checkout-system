3cdf63c02785ab8b5ae258c6da228692de4a6c84
Łukasz Sowa <contact@lukaszsowa.pl>
2015-06-11 21:07:08 +0200

    Implementing offers.

--- src/main/scala/checkout/cart/Offer.scala
+++ src/main/scala/checkout/cart/Offer.scala
@@ -1,13 +1,25 @@
 package checkout.cart
 
+import checkout.product.ProductId
+
 case class Discount(discount: BigDecimal) extends AnyVal
 
 trait Offer extends (Cart => Discount)
 
 object GratisApple extends Offer {
-  override def apply(cart: Cart): Discount = ???
+  override def apply(cart: Cart): Discount = {
+    cart.products.filter(_.id == ProductId("apple")).toList match {
+      case apples @ (x :: _) => Discount(x.price.price * (apples.length / 2))
+      case List() => Discount(BigDecimal(0))
+    }
+  }
 }
 
 object ThreeOrangesForTwo extends Offer {
-  override def apply(cart: Cart): Discount = ???
+  override def apply(cart: Cart): Discount = {
+    cart.products.filter(_.id == ProductId("orange")).toList match {
+      case oranges @ (x :: _) => Discount(x.price.price * (oranges.length / 3))
+      case List() => Discount(BigDecimal(0))
+    }
+  }
 }
