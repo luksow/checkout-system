b825d25b05b559bf22a8a158c98579eea4a8ddbe
Łukasz Sowa <contact@lukaszsowa.pl>
2015-06-11 21:19:28 +0200

    Adding CartService specs around offers.

--- src/test/scala/checkout/cart/CartServiceSpec.scala
+++ src/test/scala/checkout/cart/CartServiceSpec.scala
@@ -7,6 +7,7 @@ class CartServiceSpec extends FlatSpec with Matchers {
   val appleProduct = Product(ProductId("apple"), Price(BigDecimal("0.60")))
   val orangeProduct = Product(ProductId("orange"), Price(BigDecimal("0.25")))
   val cartService = new CartService
+  val discountedCartService = new CartService(List(ThreeOrangesForTwo, GratisApple))
 
   "Empty cart" should "be worth 0" in {
     cartService.checkout(Cart.empty).total should equal(BigDecimal(0))
@@ -21,5 +22,14 @@ class CartServiceSpec extends FlatSpec with Matchers {
 
     val cartWithManyProducts = Cart(Seq(appleProduct, appleProduct, orangeProduct, appleProduct))
     cartService.checkout(cartWithManyProducts).total should equal(BigDecimal("2.05"))
+  }
+
+  "Cart with products which service has discounts" should "calculate price correctly" in {
+    discountedCartService.checkout(Cart(List(appleProduct, orangeProduct))).total should equal(BigDecimal("0.85"))
+    discountedCartService.checkout(Cart(List(appleProduct, orangeProduct, appleProduct))).total should equal(BigDecimal("0.85"))
+    discountedCartService.checkout(Cart(List(appleProduct, orangeProduct, orangeProduct, orangeProduct))).total should equal(BigDecimal("1.10"))
+    discountedCartService.checkout(Cart(List(appleProduct, appleProduct, appleProduct, appleProduct,
+      orangeProduct, orangeProduct, orangeProduct, orangeProduct, orangeProduct, orangeProduct))).total should equal(BigDecimal("2.20"))
+
   }
  }
