7dbdd9e483d1c34826d4087a9fc5b3e6607c6ffe
Łukasz Sowa <contact@lukaszsowa.pl>
2015-06-11 20:15:07 +0200

    Implementing checkout.

--- src/main/scala/checkout/cart/CartService.scala
+++ src/main/scala/checkout/cart/CartService.scala
@@ -3,7 +3,7 @@ package checkout.cart
 class CartService {
   import CartService._
 
-  def checkout(cart: Cart): Total = ???
+  def checkout(cart: Cart): Total = Total(cart.products.map(_.price.price).fold(BigDecimal(0))(_ + _))
 }
 
 object CartService {
